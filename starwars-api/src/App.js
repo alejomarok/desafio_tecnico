4801481cfe96e988517a2f577cd7d4accca9cd03
Alejo Marco <alejom1999@gmail.com>
2022-11-23 16:38:36 -0300

    desarrolle el useEffect trayendo la data de las apis

--- starwars-api/src/App.js
+++ starwars-api/src/App.js
@@ -2,6 +2,31 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = res.json();
+      setPeople(data.results)
+    }
+    async function fetchPlanets() {
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = res.json();
+      setPlanets(data.results)
+    }
+
+    fetchPeople();
+    fetchPlanets();
+
+  }, [])
+
+  console.log ('people', people);
+  console.log ('planets', planets);
+
   return (
     <div className="App">
       hola
